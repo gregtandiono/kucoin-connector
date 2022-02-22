f87a9a34377f2b39a66da66aca968f87025fcca6
Gregory Tandiono <gregtandiono@gmail.com>
2022-02-22 19:35:33 +0800

    Establish connection and able to subscribe to a symbol

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -24,34 +25,124 @@ type TokenResponse struct {
 	} `json:"data"`
 }
 
+type Ping struct {
+	Id   string `json:"id"`
+	Type string `json:"type"`
+}
+
+type SubscriptionRequest struct {
+	Id             int    `json:"id"`
+	Type           string `json:"type"`
+	Topic          string `json:"topic"`
+	PrivateChannel bool   `json:"privateChannel"`
+	Response       bool   `json:"response"`
+}
+
 func main() {
 	r, err := http.Post("https://api.kucoin.com/api/v1/bullet-public", "application/json", nil)
 	if err != nil {
-		panic(err)
+		log.Fatal("Unable to retrieve public token", err)
 	}
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
 
 	if err != nil {
-		panic(err)
+		log.Fatal("Public token error", err)
 	}
 
 	var tokenResp TokenResponse
 	if err := json.Unmarshal(body, &tokenResp); err != nil {
-		panic(err)
+		log.Fatal("Unable to parse response body", err)
 	}
 
-	wsUrl := fmt.Sprintf("wss://ws-api.kucoin.com/endpoint?token=%s&[connectId=%s]", tokenResp.Data.Token, "loremipsumdolorsitamet78436")
-	c, _, err := websocket.DefaultDialer.Dial(wsUrl, nil)
+	connectId := "loremipsumdolorsitamet78436"
+
+	u := fmt.Sprintf("wss://ws-api.kucoin.com/endpoint?token=%s&[connectId=%s]", tokenResp.Data.Token, connectId)
+	c, _, err := websocket.DefaultDialer.Dial(u, nil)
+
 	if err != nil {
-		panic(err)
+		log.Fatal("dial:", err)
 	}
+	defer c.Close()
 
-	_, message, err := c.ReadMessage()
-	log.Println(string(message))
-	if err != nil {
-		panic(err)
-	}
+	messageType := make(chan int, 1)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// Message Reader & Handler
+	go func() {
+		for {
+			mt, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read err:", err)
+			}
+			messageType <- mt
+			// log.Println(string(message))
+		}
+	}()
+
+	// Ping
+	// go func() {
+	// 	m := <-messageType
+
+	// 	b, err := json.Marshal(Ping{Id: connectId, Type: "ping"})
+	// 	if err != nil {
+	// 		log.Println("err", err)
+	// 		return
+	// 	}
+	// 	writeErr := c.WriteMessage(m, b)
+	// 	if writeErr != nil {
+	// 		log.Println("Unable to ping ws server")
+	// 		return
+	// 	}
+	// }()
+
+	// Subscribe
+	go func() {
+		log.Println("subscribing")
+		m := <-messageType
+		b, err := json.Marshal(SubscriptionRequest{
+			Id:             1545910660739,
+			Type:           "subscribe",
+			Topic:          "/market/ticker:BTC-USDT",
+			PrivateChannel: false,
+			Response:       true,
+		})
+		if err != nil {
+			log.Println("err", err)
+			return
+		}
+
+		writeErr := c.WriteMessage(m, b)
+		if writeErr != nil {
+			log.Println("Unable to subscribe to ticker")
+			return
+		}
+	}()
+
+	pingInterval := tokenResp.Data.InstanceServers[0].PingInterval
+	log.Println("ping interval", time.Duration(pingInterval))
+
+	ticker := time.NewTicker(time.Second * 10)
+	defer ticker.Stop()
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				b, err := json.Marshal(Ping{Id: connectId, Type: "ping"})
+				if err != nil {
+					log.Println("err", err)
+					return
+				}
+				writeErr := c.WriteMessage(1, b)
+				if writeErr != nil {
+					log.Println("Unable to ping ws server")
+					return
+				}
+			case <-messageType:
+				// log.Println("message type inferred")
+			}
+		}
+	}()
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
