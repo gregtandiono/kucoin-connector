a5f63ac30921bd2309bfaddaf4e6c2b094feeee6
Gregory Tandiono <gregtandiono@gmail.com>
2022-02-24 08:31:50 +0800

    Abstract payload transformer

--- connector/kukoin.go
+++ connector/kukoin.go
@@ -6,10 +6,17 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
+	"github.com/shopspring/decimal"
 )
+
+type Ping struct {
+	Id   string `json:"id"`
+	Type string `json:"type"`
+}
 
 type SubscriptionRequest struct {
 	Id             string `json:"id"`
@@ -31,6 +38,64 @@ type TokenResponse struct {
 		} `json:"instanceServers"`
 		Token string `json:"token"`
 	} `json:"data"`
+}
+
+type KucoinInstrument struct {
+	Exchange      string `json:"exchange"`
+	Symbol        string `json:"symbol"`
+	BaseCurrency  string `json:"base_currency"`
+	QuoteCurrency string `json:"quote_currency"`
+}
+type TickerRaw struct {
+	Type    string `json:"type"`
+	Topic   string `json:"topic"`
+	Subject string `json:"subject"`
+	Data    struct {
+		BestAsk string `json:"bestAsk"`
+		BestBid string `json:"bestBid"`
+		Time    int    `json:"time"`
+	} `json:"data"`
+}
+
+type TickerData struct {
+	AskPrice decimal.Decimal `json:"ask_price"`
+	BidPrice decimal.Decimal `json:"bid_price"`
+	Time     int             `json:"raw"`
+}
+
+type Ticker struct {
+	Type       string           `json:"type"`
+	Instrument KucoinInstrument `json:"instrument"`
+	Data       TickerData       `json:"data"`
+}
+
+type KlineRaw struct {
+	Type    string `json:"type"`
+	Topic   string `json:"topic"`
+	Subject string `json:"subject"`
+	Data    struct {
+		Symbol  string   `json:"symbol"`
+		Candles []string `json:"candles"`
+		Time    int      `json:"time"`
+	} `json:"data"`
+}
+
+type KlineData struct {
+	IntervalSeconds int             `json:"interval_seconds"`
+	StartTime       int             `json:"start_time"`
+	EndTime         int             `json:"end_time"`
+	Open            decimal.Decimal `json:"open"`
+	High            decimal.Decimal `json:"high"`
+	Low             decimal.Decimal `json:"low"`
+	Close           decimal.Decimal `json:"close"`
+	VolumeBase      decimal.Decimal `json:"volume_base"`
+	VolumeQuote     decimal.Decimal `json:"volume_quote"`
+}
+
+type Kline struct {
+	Type       string           `json:"type"`
+	Instrument KucoinInstrument `json:"instrument"`
+	Data       KlineData        `json:"data"`
 }
 
 func getToken() (token string) {
@@ -72,10 +137,88 @@ func InitKucoinListener(c *websocket.Conn, t chan []byte) {
 	}
 }
 
-func SubscribeToTopic(c *websocket.Conn, s SubscriptionRequest) (err error) {
+func PingServer(c *websocket.Conn, id string) (err error) {
+	b, _ := json.Marshal(Ping{
+		Id:   id,
+		Type: "ping",
+	})
+	err = c.WriteMessage(websocket.TextMessage, b)
+	c.PingHandler()
+	return
+}
+
+func SubscribeToTopic(c *websocket.Conn, topic string, id string) (err error) {
+	s := SubscriptionRequest{
+		Id:             id,
+		Type:           "subscribe",
+		Topic:          topic,
+		PrivateChannel: false,
+		Response:       true,
+	}
 	b, _ := json.Marshal(s)
 	err = c.WriteMessage(websocket.TextMessage, b)
 
+	return
+}
+
+func ProcessRawTickerData(d []byte) (result Ticker) {
+	var raw TickerRaw
+	json.Unmarshal(d, &raw)
+
+	askPrice, _ := decimal.NewFromString(raw.Data.BestAsk)
+	bidPrice, _ := decimal.NewFromString(raw.Data.BestBid)
+
+	result = Ticker{
+		Type: "ticker",
+		Instrument: KucoinInstrument{
+			Exchange:      "kucoin",
+			Symbol:        raw.Subject,
+			BaseCurrency:  "USD",
+			QuoteCurrency: "USD",
+		},
+		Data: TickerData{
+			AskPrice: askPrice,
+			BidPrice: bidPrice,
+			Time:     raw.Data.Time,
+		},
+	}
+	return
+}
+
+func ProcessRawKlineData(d []byte) (result Kline) {
+	var raw KlineRaw
+	json.Unmarshal(d, &raw)
+
+	startTime, _ := strconv.Atoi(raw.Data.Candles[0])
+	endTime := startTime + 3600
+
+	openPrice, _ := decimal.NewFromString(raw.Data.Candles[1])
+	closePrice, _ := decimal.NewFromString(raw.Data.Candles[2])
+	highPrice, _ := decimal.NewFromString(raw.Data.Candles[3])
+	lowPrice, _ := decimal.NewFromString(raw.Data.Candles[4])
+	volumeBase, _ := decimal.NewFromString(raw.Data.Candles[5])
+	volumeQuote, _ := decimal.NewFromString(raw.Data.Candles[6])
+
+	result = Kline{
+		Type: "ohlc",
+		Instrument: KucoinInstrument{
+			Exchange:      "kucoin",
+			Symbol:        raw.Data.Symbol,
+			BaseCurrency:  "USD",
+			QuoteCurrency: "USD",
+		},
+		Data: KlineData{
+			IntervalSeconds: 3600,
+			StartTime:       startTime,
+			EndTime:         endTime,
+			Open:            openPrice,
+			High:            highPrice,
+			Low:             lowPrice,
+			Close:           closePrice,
+			VolumeBase:      volumeBase,
+			VolumeQuote:     volumeQuote,
+		},
+	}
 	return
 }
 
--- main.go
+++ main.go
@@ -7,84 +7,18 @@ import (
 	"kucoin-ws-connector/connector"
 	"log"
 	"net/http"
-	"strconv"
 	"strings"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
-	"github.com/shopspring/decimal"
 )
-
-type Ping struct {
-	Id   string `json:"id"`
-	Type string `json:"type"`
-}
 
 type Symbol struct {
 	Code string `json:"code"`
 	Data []struct {
 		Symbol string `json:"symbol"`
 	} `json:"data"`
-}
-
-type TickerRaw struct {
-	Type    string `json:"type"`
-	Topic   string `json:"topic"`
-	Subject string `json:"subject"`
-	Data    struct {
-		BestAsk string `json:"bestAsk"`
-		BestBid string `json:"bestBid"`
-		Time    int    `json:"time"`
-	} `json:"data"`
-}
-
-type KlineRaw struct {
-	Type    string `json:"type"`
-	Topic   string `json:"topic"`
-	Subject string `json:"subject"`
-	Data    struct {
-		Symbol  string   `json:"symbol"`
-		Candles []string `json:"candles"`
-		Time    int      `json:"time"`
-	} `json:"data"`
-}
-
-type KucoinInstrument struct {
-	Exchange      string `json:"exchange"`
-	Symbol        string `json:"symbol"`
-	BaseCurrency  string `json:"base_currency"`
-	QuoteCurrency string `json:"quote_currency"`
-}
-
-type TickerData struct {
-	AskPrice decimal.Decimal `json:"ask_price"`
-	BidPrice decimal.Decimal `json:"bid_price"`
-	Time     int             `json:"raw"`
-}
-
-type Ticker struct {
-	Type       string           `json:"type"`
-	Instrument KucoinInstrument `json:"instrument"`
-	Data       TickerData       `json:"data"`
-}
-
-type KlineData struct {
-	IntervalSeconds int             `json:"interval_seconds"`
-	StartTime       int             `json:"start_time"`
-	EndTime         int             `json:"end_time"`
-	Open            decimal.Decimal `json:"open"`
-	High            decimal.Decimal `json:"high"`
-	Low             decimal.Decimal `json:"low"`
-	Close           decimal.Decimal `json:"close"`
-	VolumeBase      decimal.Decimal `json:"volume_base"`
-	VolumeQuote     decimal.Decimal `json:"volume_quote"`
-}
-
-type Kline struct {
-	Type       string           `json:"type"`
-	Instrument KucoinInstrument `json:"instrument"`
-	Data       KlineData        `json:"data"`
 }
 
 func main() {
@@ -104,10 +38,6 @@ func main() {
 	if err != nil {
 		log.Fatal("Unable to dial into exchange ws:", err)
 	}
-
-	if err != nil {
-		log.Fatal("Unable to dial into exchange ws:", err)
-	}
 	defer c.Close()
 
 	ticker := time.NewTicker(time.Second * 1)
@@ -117,31 +47,29 @@ func main() {
 	rawTickerData := make(chan []byte)
 	rawKlineData := make(chan []byte)
 
-	tickerPayload := make(chan Ticker)
-	klinePayload := make(chan Kline)
+	tickerPayload := make(chan connector.Ticker)
+	klinePayload := make(chan connector.Kline)
+
+	klineConnector := make(chan *websocket.Conn)
 
 	go func() {
 		for _, s := range symbols.Data {
 			log.Println("Subscribing to kline ->", s.Symbol)
 			topic := fmt.Sprintf("/market/candles:%s_1min", s.Symbol)
 			id := uuid.NewString()
-			s := connector.SubscriptionRequest{
-				Id:             id,
-				Type:           "subscribe",
-				Topic:          topic,
-				PrivateChannel: false,
-				Response:       true,
-			}
 
 			k, _, err := connector.CreateKucoinWSClient()
 			if err != nil {
 				log.Println("Unable to subscribe ws client for kline topic->", topic)
 			}
+			defer k.Close()
 
-			sErr := connector.SubscribeToTopic(k, s)
+			sErr := connector.SubscribeToTopic(k, topic, id)
 			if sErr != nil {
 				log.Println("Unable to subscribe ws client for kline topic->", topic)
 			}
+
+			klineConnector <- k
 		}
 	}()
 
@@ -149,14 +77,8 @@ func main() {
 	go func() {
 		log.Println("subscribing to all tickers")
 		id := uuid.NewString()
-		b, _ := json.Marshal(connector.SubscriptionRequest{
-			Id:             id,
-			Type:           "subscribe",
-			Topic:          "/market/ticker:all",
-			PrivateChannel: false,
-			Response:       true,
-		})
-		err := c.WriteMessage(websocket.TextMessage, b)
+		topic := "/market/ticker:all"
+		err := connector.SubscribeToTopic(c, topic, id)
 		if err != nil {
 			log.Println("Unable to subscribe to ticker", err)
 			return
@@ -189,26 +111,7 @@ func main() {
 	// transform ticker
 	go func() {
 		for d := range rawTickerData {
-			var raw TickerRaw
-			json.Unmarshal(d, &raw)
-
-			askPrice, _ := decimal.NewFromString(raw.Data.BestAsk)
-			bidPrice, _ := decimal.NewFromString(raw.Data.BestBid)
-
-			result := Ticker{
-				Type: "ticker",
-				Instrument: KucoinInstrument{
-					Exchange:      "kucoin",
-					Symbol:        raw.Subject,
-					BaseCurrency:  "USD",
-					QuoteCurrency: "USD",
-				},
-				Data: TickerData{
-					AskPrice: askPrice,
-					BidPrice: bidPrice,
-					Time:     raw.Data.Time,
-				},
-			}
+			result := connector.ProcessRawTickerData(d)
 			tickerPayload <- result
 		}
 		close(tickerPayload)
@@ -217,42 +120,20 @@ func main() {
 	// transform kline
 	go func() {
 		for d := range rawKlineData {
-			var raw KlineRaw
-			json.Unmarshal(d, &raw)
-
-			startTime, _ := strconv.Atoi(raw.Data.Candles[0])
-			endTime := startTime + 3600
-
-			openPrice, _ := decimal.NewFromString(raw.Data.Candles[1])
-			closePrice, _ := decimal.NewFromString(raw.Data.Candles[2])
-			highPrice, _ := decimal.NewFromString(raw.Data.Candles[3])
-			lowPrice, _ := decimal.NewFromString(raw.Data.Candles[4])
-			volumeBase, _ := decimal.NewFromString(raw.Data.Candles[5])
-			volumeQuote, _ := decimal.NewFromString(raw.Data.Candles[6])
-
-			result := Kline{
-				Type: "ohlc",
-				Instrument: KucoinInstrument{
-					Exchange:      "kucoin",
-					Symbol:        raw.Data.Symbol,
-					BaseCurrency:  "USD",
-					QuoteCurrency: "USD",
-				},
-				Data: KlineData{
-					IntervalSeconds: 3600,
-					StartTime:       startTime,
-					EndTime:         endTime,
-					Open:            openPrice,
-					High:            highPrice,
-					Low:             lowPrice,
-					Close:           closePrice,
-					VolumeBase:      volumeBase,
-					VolumeQuote:     volumeQuote,
-				},
-			}
+			result := connector.ProcessRawKlineData(d)
 			klinePayload <- result
 		}
 		close(klinePayload)
+	}()
+
+	go func() {
+		id := uuid.NewString()
+		for range time.Tick(10 * time.Second) {
+			err := connector.PingServer(c, id)
+			if err != nil {
+				log.Println("Ping error", err)
+			}
+		}
 	}()
 
 	// handle payload to clients
@@ -261,9 +142,19 @@ func main() {
 			select {
 			case t := <-klinePayload:
 				log.Println(t)
-			case <-tickerPayload:
-				// case t := <-tickerPayload:
-				// 	log.Println(t)
+			case k := <-klineConnector:
+				go connector.InitKucoinListener(k, trade)
+				go func() {
+					id := uuid.NewString()
+					for range time.Tick(10 * time.Second) {
+						err := connector.PingServer(k, id)
+						if err != nil {
+							log.Println("Ping error", err)
+						}
+					}
+				}()
+			case t := <-tickerPayload:
+				log.Println(t)
 			}
 		}
 	}()
